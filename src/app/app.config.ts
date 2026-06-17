4c1694acc65e555589dc0cff8e89cb4372db1f4c
ivan5453 <ivan545353@gmail.com>
2026-06-17 03:29:45 -0300

    Comunicación con el backend completada

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { tokenInterceptor } from './core/auth/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([tokenInterceptor]))
   ]
 };
